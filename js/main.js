ce4e371c9ab5c28a2e54d0312f8776cba6e17b18
ZuhraMuk <zmu3842@gmail.com>
2022-11-23 15:08:05 +0300

    kon

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// let floor1 = document.getElementById("floor1");
-// let floor2 = document.getElementById("floor2");
-// let floor3 = document.getElementById("floor3");
-// let floor4 = document.getElementById("floor4");
-// let floor5 = document.getElementById("floor5");
-// let floor6 = document.getElementById("floor6");
-// let floor7 = document.getElementById("floor7");
-// let floor8 = document.getElementById("floor8");
-// let floor9 = document.getElementById("floor9");
-// let floor10 = document.getElementById("floor10");
-// let floor11 = document.getElementById("floor11");
-// let floor12 = document.getElementById("floor12");
-// let floor13 = document.getElementById("floor13");
-// let floor14 = document.getElementById("floor14");
-// let floor15 = document.getElementById("floor15");
-// let floor16 = document.getElementById("floor16");
 let floorUp = document.getElementById("floorUp");
 let floorDown = document.getElementById("floorDown");
 
@@ -29,10 +13,10 @@ console.log(floor7.innerText);
 let lift = document.getElementById("lift");
 let marginTop = 667;
 function calcUp() {
-  return (marginTop -= 44);
+  return (marginTop -= 44.5);
 }
 function calcDown() {
-  return (marginTop += 44);
+  return (marginTop += 44.5);
 }
 
 let elevator = {
